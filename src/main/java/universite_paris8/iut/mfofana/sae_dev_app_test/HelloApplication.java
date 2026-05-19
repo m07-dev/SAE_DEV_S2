de90ae1e36947214fac6b259b40b3e33f590f1a9
Narjesse Bouaziz <nbouaziz@iut.univ-paris8.fr>
2026-05-19 11:57:44 +0200

    Ajout class Soldat et petites modif

--- src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/HelloApplication.java
+++ src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 960);
-        stage.setTitle("Hello!");
+        stage.setTitle("Tower Defense");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/modele/Personnage.java
+++ src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/modele/Personnage.java
@@ -1,13 +1,27 @@
 package universite_paris8.iut.mfofana.sae_dev_app_test.modele;
 
-public class Personnage {
+public abstract class Personnage {
     private int x,y;
     private Terrain e;
+    private int pv;
+    private int vitesse;
+
+
+
     public Personnage(int x, int y){
         this.x = x;
-        this.y = x;
+        this.y = y;
         this.e = new Terrain();
     }
+
+   public Personnage(int x, int y, Terrain e, int pv, int v) {
+        this.x = x;
+        this.y = y;
+        this.e = new Terrain();
+        this.pv = pv;
+        this.vitesse = v;
+    }
+
 
     public int getX() {
         return x;
@@ -22,8 +36,23 @@ public class Personnage {
     public void setY(int y){
         this.y = y;
     }
+    public int getPv() {
+        return pv;
+    }
+    public void setPv(int nvPv) {
+        this.pv = Math.max(0,pv);
+    }
+
 
     public boolean horsDesLimites(){
         return this.getX() < 0 ||this.getX() >=  32*30 || this.getY() < 0 || this.getY() >= 30*32 ;
     }
+
+    public boolean estMort() {
+        return this.pv == 0;
+    }
+
+
+
+
 }
