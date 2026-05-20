366a4f3ecc94ea27c4479a28f36c2e9228895297
Narjesse Bouaziz <nbouaziz@iut.univ-paris8.fr>
2026-05-20 16:31:37 +0200

    Code des fonctions de la classe Bobomb

--- src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/modele/Brute.java
+++
@@ -1,7 +0,0 @@
-package universite_paris8.iut.mfofana.sae_dev_app_test.modele;
-
-public class Brute extends Personnage {
-    public Brute(int x, int y, Terrain e, int pv, int v) {
-        super(x, y, e, pv, v);
-    }
-}
