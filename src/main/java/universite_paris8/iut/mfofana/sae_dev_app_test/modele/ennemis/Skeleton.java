6c4c8f4f671696276fbae8468a5ede11b83c48b7
Narjesse Bouaziz <nbouaziz@iut.univ-paris8.fr>
2026-06-02 11:13:07 +0200

    Ajout de nouveaux soldats

---
+++ src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/modele/ennemis/Skeleton.java
@@ -0,0 +1,9 @@
+package universite_paris8.iut.mfofana.sae_dev_app_test.modele.ennemis;
+
+import universite_paris8.iut.mfofana.sae_dev_app_test.modele.Terrain;
+
+public class Skeleton extends Personnage{
+    public Skeleton(double x, double y, Terrain terrain, int pv, int vitesse) {
+        super(x, y, terrain, pv, vitesse);
+    }
+}
