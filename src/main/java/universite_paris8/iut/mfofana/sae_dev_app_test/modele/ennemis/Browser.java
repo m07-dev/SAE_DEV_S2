2b3aa4251c4f1dc200ff55225bf12a8cb60bc956
Narjesse Bouaziz <nbouaziz@iut.univ-paris8.fr>
2026-06-11 11:24:06 +0200

    Ajout des derniers ennemis

---
+++ src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/modele/ennemis/Browser.java
@@ -0,0 +1,12 @@
+package universite_paris8.iut.mfofana.sae_dev_app_test.modele.ennemis;
+
+import javafx.geometry.Point2D;
+import universite_paris8.iut.mfofana.sae_dev_app_test.modele.Terrain;
+
+import java.util.List;
+
+public class Browser extends Ennemis {
+    public Browser(double x, double y, Terrain terrain, int pv, int vitesse, List<Point2D> chemin, Point2D cible) {
+        super(x, y, terrain, pv, vitesse, chemin, cible);
+    }
+}
