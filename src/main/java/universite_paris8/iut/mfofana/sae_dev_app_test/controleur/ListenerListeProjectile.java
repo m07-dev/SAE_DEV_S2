3589d3e7e52d548e5386115899210705d854bcf5
Mohamed Fofana <mfofana@iut.univ-paris8.fr>
2026-06-12 17:21:08 +0200

    ajout de la classe projectile

---
+++ src/main/java/universite_paris8/iut/mfofana/sae_dev_app_test/controleur/ListenerListeProjectile.java
@@ -0,0 +1,37 @@
+package universite_paris8.iut.mfofana.sae_dev_app_test.controleur;
+
+import javafx.collections.ListChangeListener;
+import javafx.scene.layout.Pane;
+import universite_paris8.iut.mfofana.sae_dev_app_test.modele.tour.Projectile;
+import universite_paris8.iut.mfofana.sae_dev_app_test.vue.ProjectileVue;
+
+import java.util.HashMap;
+
+public class ListernerListeProjectile implements ListChangeListener<Projectile> {
+    private HashMap<Projectile, ProjectileVue> affichageProjectile = new HashMap<>();
+    private Pane pane;
+
+    public ListernerListeProjectile(Pane pane, HashMap<Projectile,ProjectileVue > affichageProjectile){
+        this.pane = pane;
+        this.affichageProjectile = affichageProjectile;
+    }
+    @Override
+    public void onChanged(Change<? extends Projectile> changement) {
+        while (changement.next()){
+            if(changement.wasAdded()){
+                for (Projectile projectile : changement.getAddedSubList()){
+                    ProjectileVue p = new ProjectileVue(pane,projectile);
+                    affichageProjectile.put(projectile,p);
+                }
+            }
+            if(changement.wasRemoved()){
+                for (Projectile projectile : changement.getRemoved()){
+                    ProjectileVue p = affichageProjectile.remove(projectile);
+                    if(p != null) {
+                        pane.getChildren().remove(p);
+                    }
+                }
+            }
+        }
+    }
+}
